a222f99e7d560912f60f6c6a40c9cdd64437db2e
grace-mao23 <gmao00@stuy.edu>
2020-01-21 00:12:23 -0500

    New buffer

--- server.c
+++ server.c
@@ -298,9 +298,10 @@ int main() {
                     read(client, buffer, sizeof(buffer));
                     printf("Subserver read %s from client\n", buffer);
                     write(fd1[sub_num][1], buffer, sizeof(buffer));
-                    read(fd2[sub_num][0], buffer, sizeof(buffer));
-                    printf("Subserver read %s statements\n", buffer);
-                    write(client, buffer, sizeof(buffer));
+                    char spec_buffer[12000] = "";
+                    read(fd2[sub_num][0], spec_buffer, sizeof(spec_buffer));
+                    printf("Subserver read %s statements\n", spec_buffer);
+                    write(client, spec_buffer, sizeof(spec_buffer));
                     mode = 0;
                 }
             }
